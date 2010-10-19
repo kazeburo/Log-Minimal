2508dc4b57c3807d75af5e752d1d8a576a334dad
Masahiro Nagano <kazeburo@gmail.com>
2010-10-19 23:30:21 +0900

    initial commit

---
+++ lib/Log/Minimal.pm
@@ -0,0 +1,32 @@
+package Log::Minimal;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Minimal -
+
+=head1 SYNOPSIS
+
+  use Log::Minimal;
+
+=head1 DESCRIPTION
+
+Log::Minimal is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
