a025fb96c6a5501472463c5c1d5bba4b5f250b6f
Masahiro Nagano <kazeburo@gmail.com>
2013-12-20 16:48:18 +0900

    Checking in changes prior to tagging of version 0.17.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index eb9b240..d94607b 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Log::Minimal
    
     {{$NEXT}}
    
    +0.17 2013-12-20T07:48:02Z
    +
    +        - fix $Data::Dumper::{SortKeys => Sortkeys}
    +
     0.16 2013-08-05T02:36:22Z
    
             - add $ESCAPE_WHITESPACE variable (Thank you xaicron)

--- lib/Log/Minimal.pm
+++ lib/Log/Minimal.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Term::ANSIColor qw//;
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 our @EXPORT = map { ($_.'f', $_.'ff') } qw/crit warn info debug croak/;
 push @EXPORT, 'ddf';
 
